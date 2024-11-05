a1b91a65f376c7267a6585e0e86999dd9c8570fa
Okan Berber <okan.berber@outlook.com.tr>
2024-11-05 23:34:09 +0300

    .

---
+++ Kalitim/Kalitim/BeyazEsya.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    public abstract class BeyazEsya
+    {
+        public BeyazEsya() { }
+        public BeyazEsya(string Isim, string Marka, int Fiyat)
+        {
+            this.Isim = Isim;
+            this.Marka = Marka;
+            this.Fiyat = Fiyat;
+        }
+
+        public string Isim { get; set; }
+        public string Marka { get; set; }
+        public int Fiyat { get; set; }
+    }
+}
---
+++ Kalitim/Kalitim/CamasirMakinesi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    internal class CamasirMakinesi : BeyazEsya
+    {
+        public CamasirMakinesi() { }//Default Constructor
+        public CamasirMakinesi(string isim, string marka, int fiyat, int programSayisi, bool kurutma)
+        {
+            base.Isim = isim;
+            base.Marka = marka;
+            base.Fiyat = fiyat;
+            this.ProgramSayisi = programSayisi;
+            this.Kurutma = kurutma;
+        }
+
+        //C# Çoklu kalıtım kabul etmez
+        public int ProgramSayisi { get; set; }
+        public bool Kurutma { get; set; }
+
+    }
+}
